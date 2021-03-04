f34ff348e11a5b5cf35e3d87758b0004cc784a89
yangliang <yangliang@szca.com>
2021-03-04 10:54:05 +0800

    修改storyboard

--- 01-GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
+++ 01-GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProductsTableViewController: UIViewController{
+class ProductsTableViewController: UITableViewController{
     private var products: [Product]?
     private let identifer = "productCell"
     
@@ -23,5 +23,38 @@ class ProductsTableViewController: UIViewController{
         ]
     }
     
+    // MARK: - View Transfer
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showProduct" {
+            if let cell =  sender as? UITableViewCell,
+               let indexPath = tableView.indexPath(for: cell),
+               let productVC = segue.destination as?  ProductViewController {
+                productVC.product = products?[indexPath.row]
+            }
+        }
+    }
     
+    
+}
+
+// MARK: - UITableViewDataSource
+extension ProductsTableViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifer, for: indexPath)
+        guard let products = products else {
+            return cell
+        }
+        
+        cell.textLabel?.text = products[indexPath.row].name
+        
+        if let imageName = products[indexPath.row].cellImageName {
+            cell.imageView?.image = UIImage(named: imageName)
+        }
+        
+        return cell;
+    }
 }
